19eae3ea4eefae1a372a4dac49e436384f035c5b
Aviv Zohar <avivzo9@gmail.com>
2021-05-18 14:40:10 +0300

    heroku

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 // const authRoutes = require('./api/auth/auth.routes')
 // const userRoutes = require('./api/user/user.routes')
 const boardRoutes = require('./api/board/board.routes')
-const { connectSockets } = require('./services/socket.service')
+const {connectSockets} = require('./services/socket.service')
 
 // routes
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
@@ -50,7 +50,9 @@ app.get('/**', (req, res) => {
 })
 
 const logger = require('./services/logger.service')
-http.listen(process.env.PORT || 3030, () => {
+const port = process.env.PORT || 3030
+console.log('port:', port)
+http.listen(port, () => {
     logger.info('Server is running on port: ' + port)
 })
 
